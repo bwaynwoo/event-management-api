8c0a3a0ea25207c03bbfb6b1dedeb53f622c70e8
David Shchepkin <sshhepkin@gmail.com>
2026-04-14 11:08:06 +0300

    review: ConcurrentDictionary instead List

--- EventManagementApi/Services/EventService.cs
+++ EventManagementApi/Services/EventService.cs
@@ -1,49 +1,37 @@
+using System.Collections.Concurrent;
 using EventManagementApi.Models;
 
 namespace EventManagementApi.Services;
 
 public class EventService : IEventService
 {
-    private static List<Event> Events { get; set; } = [];
+    private static readonly ConcurrentDictionary<int, Event> Events = new();
     private static int _nextId = 1;
 
     public IReadOnlyCollection<Event> GetEvents()
     {
-        return Events;
+        return Events.Values.ToList();
     }
 
     public Event? GetEvent(int id)
     {
-        return Events.FirstOrDefault(e => e.Id == id);
+        Events.TryGetValue(id, out var eventItem);
+        return eventItem;
     }
 
     public void AddEvent(Event eventItem)
     {
         eventItem.Id = _nextId++;
-        Events.Add(eventItem);
+        Events.TryAdd(eventItem.Id,  eventItem);
     }
 
     public void UpdateEvent(int id, Event eventItem)
     {
-        foreach (var existingEvent in Events)
-        {
-            if (existingEvent.Id == id) 
-            {
-                existingEvent.Title = eventItem.Title;
-                existingEvent.Description = eventItem.Description;
-                existingEvent.StartAt = eventItem.StartAt;
-                existingEvent.EndAt = eventItem.EndAt;
-                break;
-            }
-        }
+        Events.TryUpdate(id, eventItem, Events[id]);
     }
 
     public void RemoveEvent(int id)
     {
-        var eventItem = Events.FirstOrDefault(e => e.Id == id);
-        if (eventItem != null)
-        {
-            Events.Remove(eventItem);
-        }
+        Events.TryRemove(id, out _);
     }
 }
