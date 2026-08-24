50f7f784d2e9cbe9d28df563badfe38c452a4aa4
David Shchepkin <sshhepkin@gmail.com>
2026-08-24 05:16:17 +0300

    fix warning

--- IntegrationTests/UserRepositoryTests.cs
+++ IntegrationTests/UserRepositoryTests.cs
@@ -10,8 +10,7 @@ namespace IntegrationTests;
 
 public sealed class UserRepositoryTests : IAsyncLifetime
 {
-    private readonly PostgreSqlContainer _postgres = new PostgreSqlBuilder()
-        .WithImage("postgres:16-alpine")
+    private readonly PostgreSqlContainer _postgres = new PostgreSqlBuilder("postgres:16-alpine")
         .Build();
 
     public async Task InitializeAsync() => await _postgres.StartAsync();
